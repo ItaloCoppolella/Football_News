3918c5bfdf10fda03779a137cc532aec0424b0be
Italo Coppolella <italo7094@hotmail.it>
2025-12-15 15:18:48 +0100

    import LeagueSelector.module.css + altre modifiche

--- app/components/LeagueSelector.js
+++ app/components/LeagueSelector.js
@@ -1,22 +1,22 @@
 // Permette all’utente di scegliere un campionato.
 
 import Link from "next/link";
+import styles from "./LeagueSelector.module.css";
 
 export default function LeagueSelector() {
   const leagues = [
-    {
-      id: "serie-a-2023",
-      label: "Serie A 2023/24"
-    }
+    { id: "serie-a-2023", label: "Serie A 2023/24" },
   ];
 
   return (
-    <div>
-      <h2>Seleziona un campionato</h2>
-      <ul>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Seleziona un campionato</h2>
+      <ul className={styles.list}>
         {leagues.map((league) => (
-          <li key={league.id}>
-            <Link href={`/league/${league.id}`}>{league.label}</Link>
+          <li key={league.id} className={styles.item}>
+            <Link href={`/league/${league.id}`} className={styles.link}>
+              {league.label}
+            </Link>
           </li>
         ))}
       </ul>
